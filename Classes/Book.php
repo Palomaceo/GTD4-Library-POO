f37e43a35780631d09a9738cafa1550b3856ac74
Palomaceo <maceof2020@outlook.com>
2024-10-02 14:10:44 +0200

    DONE: Book class finished

--- Classes/Book.php
+++ Classes/Book.php
@@ -3,5 +3,22 @@
 namespace Classes;
 
 class Book {
+    private string $title;
+    private string $author;
+    private int $pages;
+    private int $isbn;
+
+    public function __construct(string $title, string $author, int $pages, int $isbn) 
+    {
+        $this->title = $title;
+        $this->author = $author;
+        $this->pages = $pages;
+        $this->isbn = $isbn;
+    }
     
+    public function __toString(): string 
+    {
+        return "Title: {$this->title}, Author: {$this->author}, Pages: {$this->pages}, ISBN: {$this->isbn}" . PHP_EOL;
+    }
+
 }
